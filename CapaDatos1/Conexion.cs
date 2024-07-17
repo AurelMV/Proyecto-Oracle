cb8777a87f4eaaaf908bc18df4b94355ed2cc9ea
Kenta147 <rusbelcabrerasicus@gmail.com>
2024-07-16 20:43:42 -0500

    hey modificado el tipo de clase que se uso

---
+++ CapaDatos1/Conexion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ora
+
+namespace CapaDatos1
+{
+    internal class Conexion
+    {
+        private static Conexion Con = null;
+
+        public Conexion()
+        {
+
+
+        }
+
+        public OracleConnection CrearConexion()
+        {
+
+            OracleConnection cadena = new OracleConnection();
+            try
+            {
+                //conecion con oracle
+                cadena.ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)" +
+                                          "(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));" +
+                                          "User Id=Palola;Password=123456;";
+
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+
+                throw ex;
+            }
+            return cadena;
+
+        }
+        public static Conexion getInstancia()
+        {
+            if (Con == null)
+            {
+
+                Con = new Conexion();
+
+            }
+            return Con;
+
+        }
+    }
+}
