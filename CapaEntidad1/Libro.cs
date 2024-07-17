4f700924cbda2d82403d323db9c9a19604642fae
Kenta147 <rusbelcabrerasicus@gmail.com>
2024-07-16 21:07:00 -0500

    algo mas

---
+++ CapaEntidad1/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad1
+{
+    public class Libro
+    {
+        public int IdLibro { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int IdCategoria { get; set; }
+        public int? IdProveedor { get; set; }
+        public string Editorial { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
